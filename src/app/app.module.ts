446e9a4f9252a9722a56120a3a2a7747ab07af86
Name <249478@gmail.com>
2018-12-25 21:36:01 +0200

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { HouseComponent } from './house/house.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { PriceComponent } from './price/price.component';
+import { YardComponent } from './yard/yard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    CalendarComponent,
+    ContactsComponent,
+    HouseComponent,
+    GalleryComponent,
+    PriceComponent,
+    YardComponent
   ],
   imports: [
     BrowserModule,
